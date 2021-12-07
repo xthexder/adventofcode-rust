cb5cb057fcada9c2be970c80c4e45847f34c325e
Jacob Wirth <xthexder@users.noreply.github.com>
2021-12-07 00:32:43 -0500

    Add day1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# adventofcode-rust
-Advent of Code solutions in Rust, 2021
+# Advent of Code (in Rust)
+
+This repository is my solutions to [Advent of Code 2021](https://adventofcode.com/2021).
+
+I'm using this as an opportunity to learn Rust, so beware.
---
+++ day1/src/main.rs
@@ -0,0 +1,43 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io;
+
+fn part1(contents: &str) -> io::Result<()> {
+    let numbers = contents.lines().filter_map(|line| line.parse::<i32>().ok() );
+    let mut previous: Option<i32> = None;
+    let mut sum = 0;
+    for num in numbers {
+        if let Some(prev) = previous {
+            if num > prev {
+                sum += 1;
+            }
+        }
+        previous = Some(num);
+    }
+    println!("Part 1: {}", sum);
+    Ok(())
+}
+
+fn part2(contents: &str) -> io::Result<()> {
+    let numbers: Vec<i32> = contents.lines().filter_map(|line| line.parse::<i32>().ok() ).collect();
+    let (mut tail, mut head) = (0, 2);
+    let mut sum = 0;
+    while head < numbers.len() - 1 {
+        head += 1;
+        if numbers[head] - numbers[tail] > 0 {
+            sum += 1;
+        }
+        tail += 1;
+    }
+    println!("Part 2: {}", sum);
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let mut file = File::open("day1.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Failed to read file");
+
+    part1(&contents)?;
+    part2(&contents)
+}
